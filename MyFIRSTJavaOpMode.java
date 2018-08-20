f7d0fdf53b6b93583f52a6075eb8d8b22afdc2f7
scott <ftc14462@gmail.com>
2018-08-19 20:33:23 -0600

    reformatting

--- MyFIRSTJavaOpMode.java
+++ MyFIRSTJavaOpMode.java
@@ -9,70 +9,22 @@ import com.qualcomm.robotcore.hardware.Servo;
 @TeleOp
 public class MyFIRSTJavaOpMode extends LinearOpMode {
 
-    private
-    Gyroscope
-            imu
-            ;
-    private
-    DcMotor
-            motorTest
-            ;
-    private
-    DigitalChannel
-            digitalTouch
-            ;
-    private
-    DistanceSensor
-            sensorColorRange
-            ;
-    private
-    Servo
-            servoTest
-            ;
+    private Gyroscope imu;
+    private DcMotor motorTest;
+    private DigitalChannel digitalTouch;
+    private DistanceSensor sensorColorRange;
+    private Servo servoTest;
+
     @Override
-    public void
-    runOpMode() {
-        imu
-                =
-                hardwareMap
-                        .get(Gyroscope.
-                                        class
-                                ,
-                                "imu"
-                        );
-        motorTest
-                =
-                hardwareMap
-                        .get(DcMotor.
-                                        class
-                                ,
-                                "motorTest"
-                        );
-        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch" );
-        sensorColorRange
-                =
-                hardwareMap
-                        .get(DistanceSensor.
-                                        class
-                                ,
-                                "sensorColorRange"
-                        );
-        servoTest
-                =
-                hardwareMap
-                        .get(Servo.
-                                        class
-                                ,
-                                "servoTest"
-                        );
-        telemetry
-                .addData(
-                        "Status"
-                        ,
-                        "Initialized"
-                );
-        telemetry
-                .update();
+    public void runOpMode()
+    {
+        imu = hardwareMap.get(Gyroscope.class, "imu");
+        motorTest = hardwareMap.get(DcMotor.class,"motorTest");
+        digitalTouch = hardwareMap.get(DigitalChannel.class, "digitalTouch");
+        sensorColorRange = hardwareMap.get(DistanceSensor.class, "sensorColorRange");
+        servoTest = hardwareMap.get(Servo.class, "servoTest");
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 // Wait for the game to start (driver presses PLAY)
         waitForStart();
 // run until the end of the match (driver presses STOP)
