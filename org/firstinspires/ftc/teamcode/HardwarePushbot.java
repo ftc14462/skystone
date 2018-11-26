fe6be0e6e9ca9b473baad26dcd57a02aea69cb5d
scott <ftc14462@gmail.com>
2018-11-25 17:10:09 -0700

    We made right and left motors work. HEMLO

--- org/firstinspires/ftc/teamcode/HardwarePushbot.java
+++ org/firstinspires/ftc/teamcode/HardwarePushbot.java
@@ -72,6 +72,7 @@ public class HardwarePushbot
 
     }
 
+    //HEMLO
     /* Initialize standard Hardware interfaces */
     public void init(HardwareMap ahwMap) {
         // Save reference to Hardware map
